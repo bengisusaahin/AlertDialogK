5fdd2ac532353fe70818f90e54f670b9f3c5042e
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-13 20:33:16 +0300

    Alert Dialog

--- app/src/main/java/com/bengisusahin/alertdialogk/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/alertdialogk/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.bengisusahin.alertdialogk
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,6 +42,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun save(view : View){
+        val alert = AlertDialog.Builder(this@MainActivity)
+        alert.setTitle("Save")
+        alert.setMessage("Are you sure?")
+        alert.setPositiveButton("Yes", object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                Toast.makeText(this@MainActivity, "Saved!", Toast.LENGTH_LONG).show()
+            }
 
+        })
+        alert.setNegativeButton("No"){dialog, which ->
+            Toast.makeText(this@MainActivity, "Not Saved!", Toast.LENGTH_LONG).show()
+        }
+        alert.show()
     }
 }
